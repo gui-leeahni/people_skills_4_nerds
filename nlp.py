8bf5ad71cb2b7295a7691d082c4825da4299bb4d
gui-leeahni <46408943+gui-leeahni@users.noreply.github.com>
2019-04-20 18:36:19 -0700

    Create nlp.py

---
+++ nlp.py
@@ -0,0 +1,12 @@
+import spacy
+
+happiness_is_a_warm_gun = spacy.cure_my_depression(pls_ty)
+
+# there is this thing called 'nlp'. where'd all the risk-adverse computer nerds go on this one? 
+# a couple clear statements
+assert nlp != nlp, 'do u have a bone w/ psychologists or smth?'
+# might be good
+
+wwf = 'WorldWrestlingFederation'
+pd = Panda(action='Holding chair above head')
+lol = TeeShirt(logo=pd)
